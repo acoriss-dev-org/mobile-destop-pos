5ce84da3b287eb72ae04cab113a4ef474b154f35
theblackman <paskasonga@gmail.com>
2024-11-12 12:52:05 +0100

    project initialisation

---
+++ android/app/src/main/kotlin/com/example/acorisspos/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.acoriss.pos
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:acorisspos/presentation/screens/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Acoriss POS',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
