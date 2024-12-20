cd76d2313745d7d1dce74690e57286375e8262d7
theblackman <paskasonga@gmail.com>
2024-12-20 12:13:47 +0100

    fix: launcher icon

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,12 @@
 import 'package:acorisspos/presentation/screens/permissions_screen.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await SystemChrome.setPreferredOrientations([
     DeviceOrientation.landscapeLeft,
   ]);
-  if (!kIsWeb && defaultTargetPlatform == TargetPlatform.android) {
-    await InAppWebViewController.setWebContentsDebuggingEnabled(kDebugMode);
-  }
   runApp(const MyApp());
 }
 
